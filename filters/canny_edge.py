008ab0a1143e3e105a702917a289c369983d00ce
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-10 18:16:54 -0500

    Update canny_edge.py

--- filters/canny_edge.py
+++ filters/canny_edge.py
@@ -1,1 +1,10 @@
+import cv2
 
+def canny_edge(image_path, low=100, high=200):
+    img = cv2.imread(image_path, 0)
+    edges = cv2.Canny(img, low, high)
+    return edges
+
+if __name__ == "__main__":
+    edges = canny_edge("../images/input/sample.jpg")
+    cv2.imwrite("../images/output/canny_edge.jpg", edges)
