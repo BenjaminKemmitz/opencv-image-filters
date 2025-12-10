1e19fadb7f9ebed8c6753a40424f7be35dceab45
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-10 09:40:56 -0500

    Update gaussian_blur.py

--- filters/gaussian_blur.py
+++ filters/gaussian_blur.py
@@ -1,1 +1,16 @@
+import cv2
 
+# Reads input image, defines guassian blur
+def gaussian_blur(image_path, ksize=(5, 5), sigma=0):
+    image = cv2.imread(image_path)
+    blurred = cv2.GaussianBlur(image, ksize, sigma)
+    return blurred
+
+#Apply Gaussian Blur
+if __name__ == "__main__":
+    path = "../images/input/sample.jpg"
+    output_path = "../images/output/gaussian_blur.jpg"
+  
+    result = gaussian_blur(path)
+    cv2.imwrite(output_path, result)
+    print(f"Saved: {output_path}")
