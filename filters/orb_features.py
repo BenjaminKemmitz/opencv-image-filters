bb8372aaaeeed62d160c1fd7d870b2c294abd653
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-10 18:30:27 -0500

    Update orb_features.py

--- filters/orb_features.py
+++ filters/orb_features.py
@@ -1,1 +1,15 @@
+import cv2
+
+def orb_features(image_path, n_features=500):
+    img = cv2.imread(image_path)
+    orb = cv2.ORB_create(nfeatures=n_features)
+
+    keypoints, descriptors = orb.detectAndCompute(img, None)
+    out = cv2.drawKeypoints(img, keypoints, None, color=(0,255,0))
+
+    return out
+
+if __name__ == "__main__":
+    img = orb_features("../images/input/sample.jpg")
+    cv2.imwrite("../images/output/orb_features.jpg", img)
 
