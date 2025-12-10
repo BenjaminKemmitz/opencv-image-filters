a468cebb6b262ad984d5cc9f4bf3c2ded9e1fcb4
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-10 15:02:42 -0500

    Update and rename sobel_operator.py to sobel_edge.py

---
+++ filters/sobel_edge.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+
+def sobel_edge(image_path):
+    #Load image
+    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+    # Compute x & y gradient directions
+    # Arguments: src, ddepth, dx, dy, ksize
+    sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
+    sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
+
+    # Compute gradient magnitude G= srt(Gx^2 +Gy^2)
+    magnitude = cv2.magnitude(sobelx, sobely)
+    #min-max normalization, scales floating point mag to 0-255 range for np.unit8 image
+    magnitude = np.uint8(255 * magnitude / np.max(magnitude))
+
+    return magnitude
+
+if __name__ == "__main__":
+    path = "../images/input/sample.jpg"
+    output_path = "../images/output/sobel_edge.jpg"
+    
+    edges = sobel_edge(path)
+    cv2.imwrite(output_path, edges)
+    print(f"Saved: {output_path}")
+
