4af16ca232c753e647383c58d73bc23c2640e7c1
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-10 15:30:12 -0500

    Update median.py

--- filters/median.py
+++ filters/median.py
@@ -1,1 +1,12 @@
+import cv2
 
+# Define median filter function, run through medianBlur operator
+def median_filter(image_path, ksize=5):
+    img = cv2.imread(image_path)
+    filtered = cv2.medianBlur(img, ksize)
+    return filtered
+
+# Check if main, run function
+if __name__ == "__main__":
+    result = median_filter("../images/input/sample.jpg")
+    cv2.imwrite("../images/output/median_filter.jpg", result)
