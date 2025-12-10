96e1fd0af18be0d62c206214c9046a40403bbb10
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-10 18:31:14 -0500

    Update fast_features.py

--- filters/fast_features.py
+++ filters/fast_features.py
@@ -1,1 +1,16 @@
+import cv2
+
+def fast_features(image_path):
+    img = cv2.imread(image_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    fast = cv2.FastFeatureDetector_create()
+    keypoints = fast.detect(gray, None)
+
+    out = cv2.drawKeypoints(img, keypoints, None, color=(255,0,0))
+    return out
+
+if __name__ == "__main__":
+    img = fast_features("../images/input/sample.jpg")
+    cv2.imwrite("../images/output/fast_features.jpg", img)
 
