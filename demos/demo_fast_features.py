630ce54e63ac84cd05efefedc9b23db87e04b2c2
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-10 19:10:30 -0500

    Update demo_fast_features.py

--- demos/demo_fast_features.py
+++ demos/demo_fast_features.py
@@ -1,1 +1,10 @@
+from filters.fast_features import fast_features
+import matplotlib.pyplot as plt
+import cv2
 
+img = fast_features("../images/input/sample.jpg")
+
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.title("FAST Features Demo")
+plt.axis("off")
+plt.show()
