6125033828d55b9a2681ff69a04924c2d526ade5
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-10 19:17:33 -0500

    Update demo_median_filter.py

--- demos/demo_median_filter.py
+++ demos/demo_median_filter.py
@@ -1,1 +1,10 @@
+from filters.median_filter import median_filter
+import matplotlib.pyplot as plt
+import cv2
 
+img = median_filter("../images/input/sample.jpg")
+
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.title("Median Filter Demo")
+plt.axis("off")
+plt.show()
