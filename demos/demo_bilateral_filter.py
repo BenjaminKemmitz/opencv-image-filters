bdb0894d5162c1842f3fae6e57340ac1410d7e5e
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-10 19:04:08 -0500

    Update demo_bilateral_filter.py

--- demos/demo_bilateral_filter.py
+++ demos/demo_bilateral_filter.py
@@ -2,9 +2,9 @@ from filters.bilateral_filter import bilateral_filter
 import matplotlib.pyplot as plt
 import cv2
 
-img = gaussian_blur("../images/input/sample.jpg")
+img = bilateral_filter("../images/input/sample.jpg")
 
 plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-plt.title("Gaussian Blur Demo")
+plt.title("Bilateral Filter Demo")
 plt.axis("off")
 plt.show()
