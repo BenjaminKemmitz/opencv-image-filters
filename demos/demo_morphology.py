74fd7695030bad31ffd2082312310435b822ba94
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-10 19:21:25 -0500

    Update demo_morphology.py

--- demos/demo_morphology.py
+++ demos/demo_morphology.py
@@ -1,1 +1,10 @@
+from filters.morphology import morphology
+import matplotlib.pyplot as plt
+import cv2
 
+img = morphology("../images/input/sample.jpg")
+
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.title("Morphology Demo")
+plt.axis("off")
+plt.show()
