3f8a5902199839f530afad6bdf38ce899702b74d
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-10 19:12:09 -0500

    Update demo_harris_corners.py

--- demos/demo_harris_corners.py
+++ demos/demo_harris_corners.py
@@ -1,1 +1,10 @@
+from filters.harris_corners import harris_corners
+import matplotlib.pyplot as plt
+import cv2
 
+img = harris_corners("../images/input/sample.jpg")
+
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.title("Gaussian Blur Demo")
+plt.axis("off")
+plt.show()
