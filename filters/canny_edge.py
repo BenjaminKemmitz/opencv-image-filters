5738c0936c6c3b8aa04ec978e79813d878b27b28
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-14 17:59:51 -0500

    Update canny_edge.py

--- filters/canny_edge.py
+++ filters/canny_edge.py
@@ -1,10 +1,14 @@
 import cv2
 
-def canny_edge(image_path, low=100, high=200):
-    img = cv2.imread(image_path, 0)
-    edges = cv2.Canny(img, low, high)
-    return edges
+def canny_edge(image, low=100, high=200):
+    if image is None:
+        raise ValueError("Input image is None")
+        
+    return cv2.Canny(image, low, high)
 
 if __name__ == "__main__":
     edges = canny_edge("../images/input/sample.jpg")
     cv2.imwrite("../images/output/canny_edge.jpg", edges)
+
+
+
