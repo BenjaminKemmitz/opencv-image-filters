0c7cbf41d74a6f6e1bbafa6119b43ee50ee42120
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-14 17:51:07 -0500

    Update median.py

--- filters/median.py
+++ filters/median.py
@@ -1,7 +1,7 @@
 import cv2
 
 # Define median filter function, run through medianBlur operator
-def median(image, ksize=7):
+def median(image, ksize=5):
     if image is None:
         raise ValueError("Input image is None")
     
