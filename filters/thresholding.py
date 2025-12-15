c5fe9eb48a01ccbbf486211c0d84a431d91dcc75
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-14 22:47:46 -0500

    Update thresholding.py

--- filters/thresholding.py
+++ filters/thresholding.py
@@ -1,8 +1,9 @@
 import cv2
 
 def basic_threshold(image_path, thresh=128):
-    _, binary = cv2.threshold(image, thresh, 255, cv2.THRESH_BINARY)
-    return binary
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)   
+    _, binary = cv2.threshold(gray, thresh, 255, cv2.THRESH_BINARY)
+    return cv2.cvtColor(binary, cv2.COLOR_GRAY2BGR)
 
 if __name__ == "__main__":
     result = basic_threshold("../images/input/sample.jpg")
