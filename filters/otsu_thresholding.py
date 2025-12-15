c12646c1f7ffff2d95ea4d700e2e9cf48965c25a
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-14 22:43:04 -0500

    Update otsu_thresholding.py

--- filters/otsu_thresholding.py
+++ filters/otsu_thresholding.py
@@ -3,7 +3,7 @@ import cv2
 def otsu_thresholding(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
-    _, binary = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    _, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     return cv2.cvtColor(binary, cv2.COLOR_GRAY2BGR)
 
 if __name__ == "__main__":
