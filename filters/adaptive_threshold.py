b5e767d8ed50e18422deae7cc50caf9e3a0fa592
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-14 20:19:32 -0500

    Update adaptive_threshold.py

--- filters/adaptive_threshold.py
+++ filters/adaptive_threshold.py
@@ -1,9 +1,8 @@
 import cv2
 
 def adaptive_threshold(image):
-    img = cv2.imread(image, 0)
     binary = cv2.adaptiveThreshold(
-        img, 255,
+        image, 255,
         cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
         cv2.THRESH_BINARY,
         11, 2
