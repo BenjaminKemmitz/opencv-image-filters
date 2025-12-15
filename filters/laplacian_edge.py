8ab5372eede14f451b70b3ed6ed75321d52b3592
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-14 20:24:00 -0500

    Update laplacian_edge.py

--- filters/laplacian_edge.py
+++ filters/laplacian_edge.py
@@ -1,9 +1,8 @@
 import cv2
 import numpy as np
 
-def laplacian_edge(image_path):
-    img = cv2.imread(image_path, 0)
-    lap = cv2.Laplacian(img, cv2.CV_64F)
+def laplacian_edge(image):
+    lap = cv2.Laplacian(image, cv2.CV_64F)
     lap = np.uint8(np.absolute(lap))
     return lap
 
