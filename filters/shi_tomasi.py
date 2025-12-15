c1f01a1f4466e6d73164a89f2d0bd1099bd30ab7
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-14 20:29:35 -0500

    Update shi_tomasi.py

--- filters/shi_tomasi.py
+++ filters/shi_tomasi.py
@@ -1,16 +1,15 @@
 import cv2
 
-def shi_tomasi(image_path, max_corners=100):
-    img = cv2.imread(image_path)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def shi_tomasi(image, max_corners=100):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     corners = cv2.goodFeaturesToTrack(gray, max_corners, 0.01, 10)
     corners = corners.astype(int)
 
     for x, y in corners.reshape(-1, 2):
-        cv2.circle(img, (x, y), 4, (0, 255, 0), -1)
+        cv2.circle(image, (x, y), 4, (0, 255, 0), -1)
 
-    return img
+    return image
 
 if __name__ == "__main__":
     img = shi_tomasi("../images/input/sample.jpg")
