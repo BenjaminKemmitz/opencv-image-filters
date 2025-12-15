a66d5474eed958fc01c5bcf77383be357b1111b9
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-14 20:28:34 -0500

    Update otsu_thresholding.py

--- filters/otsu_thresholding.py
+++ filters/otsu_thresholding.py
@@ -1,8 +1,7 @@
 import cv2
 
-def otsu_thresholding(image_path):
-    img = cv2.imread(image_path, 0)
-    _, binary = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+def otsu_thresholding(image):
+    _, binary = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     return binary
 
 if __name__ == "__main__":
