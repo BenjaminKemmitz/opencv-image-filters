171fc0fe75db329e4553add14d1953fb8797d346
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-14 21:53:36 -0500

    Update canny_edge.py

--- filters/canny_edge.py
+++ filters/canny_edge.py
@@ -1,6 +1,7 @@
 import cv2
 
 def canny_edge(image, low=100, high=200):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     if image is None:
         raise ValueError("Input image is None")
         
