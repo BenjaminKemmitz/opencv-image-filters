b50e77a5a95bb1a5038034f6edbd816ffc26f975
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-14 20:22:21 -0500

    Update harris_corners.py

--- filters/harris_corners.py
+++ filters/harris_corners.py
@@ -1,15 +1,13 @@
 import cv2
 import numpy as np
 
-def harris_corners(image_path):
-    img = cv2.imread(image_path)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def harris_corners(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = np.float32(gray)
 
     corners = cv2.cornerHarris(gray, 2, 3, 0.04)
-    img[corners > 0.01 * corners.max()] = [0, 0, 255]
-
-    return img
+   
+    return  image[corners > 0.01 * corners.max()] = [0, 0, 255]
 
 if __name__ == "__main__":
     img = harris_corners("../images/input/sample.jpg")
