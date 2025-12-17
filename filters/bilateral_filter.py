c7ad08da0b1063957f2578e49ea63e5078eccfe3
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-17 18:28:54 -0500

    Update bilateral_filter.py

--- filters/bilateral_filter.py
+++ filters/bilateral_filter.py
@@ -1,9 +1,7 @@
+# Bilateral Filter Smooths images while preserving edges by combining spatial and intensity information—ideal for denoising without blurring edges.
+
 import cv2
 
 def bilateral_filter(image, d=9, sigmaColor=75, sigmaSpace=75):
     filtered = cv2.bilateralFilter(image, d, sigmaColor, sigmaSpace)
     return filtered
-
-if __name__ == "__main__":
-    result = bilateral_filter("../images/input/sample.jpg")
-    cv2.imwrite("../images/output/bilateral_filter.jpg", result)
