2341cf86af133b902bc95798a5e1ba084e6f8db1
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-17 18:39:50 -0500

    Update thresholding.py

--- filters/thresholding.py
+++ filters/thresholding.py
@@ -1,11 +1,8 @@
+# Basic Thresholding Converts an image to binary by comparing pixel intensity to a fixed threshold value.
+
 import cv2
 
 def basic_threshold(image, thresh=128):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)   
     _, binary = cv2.threshold(gray, thresh, 255, cv2.THRESH_BINARY)
     return cv2.cvtColor(binary, cv2.COLOR_GRAY2BGR)
-
-if __name__ == "__main__":
-    result = basic_threshold("../images/input/sample.jpg")
-    cv2.imwrite("../images/output/basic_threshold.jpg", result)
-
