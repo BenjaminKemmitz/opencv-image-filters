a7ab1c595d03fd3c71e8e014a29b3b652684b5eb
Benjamin Kemmitz <104531569+BenjaminKemmitz@users.noreply.github.com>
2025-12-17 18:37:46 -0500

    Update otsu_thresholding.py

--- filters/otsu_thresholding.py
+++ filters/otsu_thresholding.py
@@ -1,3 +1,5 @@
+# Otsu’s Thresholding Automatically computes the optimal threshold by maximizing inter-class variance—ideal when the image histogram is bimodal.
+
 import cv2
 
 def otsu_thresholding(image):
@@ -5,8 +7,3 @@ def otsu_thresholding(image):
     
     _, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     return cv2.cvtColor(binary, cv2.COLOR_GRAY2BGR)
-
-if __name__ == "__main__":
-    result = otsu_threshold("../images/input/sample.jpg")
-    cv2.imwrite("../images/output/otsu_threshold.jpg", result)
-
